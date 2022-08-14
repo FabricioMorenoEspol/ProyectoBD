b11c405bc9d86622e78f8c0f0a744d60b63a4e24
FabricioMorenoEspol <fbmoreno@espol.edu.ec>
2022-08-14 07:26:55 -0500

    Implementacion del menu del programa

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import pymysql
 from dotenv import load_dotenv
 import os
 
-import interfaces.GenerarReporte as c1
+
+import interfaces.CrearCampana as c1
 import interfaces.GenerarReporte as c2
 import interfaces.ActualizarDatos as c3
 import interfaces.EliminarDatos as c4
@@ -24,60 +25,31 @@ engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{IP}:{PORT}/FacebookA
 connection = engine.connect()
 
 
-print("Bienvenidos al sistema")
-print("Qué proceso desea realizar:")
-print("1.Crear una campaña")
-print("2.Generar un reporte")
-print("3.Actualizar Datos")
-print("4.Eliminar datos")
+opcElegida = 0
 
+while not opcElegida == 5:
+    
+    print("Bienvenidos al sistema")
+    print("Qué proceso desea realizar:")
+    print("1.Crear una campaña")
+    print("2.Generar un reporte")
+    print("3.Actualizar Datos")
+    print("4.Eliminar datos")
+    print("5.Salir")
 
+    opcElegida = int(input("Seleccione su respuesta: "))
 
-""" Reporte del total a pagar por cada Conjunto de Anuncios """
-
-print("Reporte del total a pagar por cada Conjunto de Anuncios \nNombre | Total")
-
-consulta1 = "select CA.nombre, sum(Cantidad_Pago) from ANUNCIO A join CONJUNTO_DE_ANUNCIOS CA on A.ID_ConjuntoAnuncio = CA.ID_ConjuntoAnuncio join FACTURA F on A.ID_Anuncio=F.ID_Anuncio group by CA.Nombre;"
-results1 = connection.execute(consulta1).fetchmany(size=10)
-
-for nombre,Total in results1:
-    print(nombre +"----->"+"$"+str(Total))
-
-
-""" Reporte del anuncio cuya factura fue la mas alta """
-
-print("\nReporte del anuncio cuya factura fue la mas alta \nNombre | Total")
-
-consulta2 = "select A.Nombre, max(F.Cantidad_Pago) from ANUNCIO A join FACTURA F on A.ID_Anuncio=F.ID_Anuncio group by A.Nombre"
-results2 = connection.execute(consulta2).fetchmany(size=10)
-
-for nombre,Total in results2:
-    print(nombre+"----->"+"$"+str(Total))
-
-
-
-
-""" Reporte de cantidad de anuncios publicados por cada campaña """ 
-
-print("\nReporte de cantidad de anuncios publicados por cada campaña \nCampana | Total anuncios publicados ")
-
-consulta3 = "select cam.Nombre,count(ID_Anuncio) from CAMPAÑA as cam, CONJUNTO_DE_ANUNCIOS as con, ANUNCIO anun where cam.ID_Campaña=con.ID_Campaña and con.ID_ConjuntoAnuncio = anun.ID_ConjuntoAnuncio group by cam.Nombre"
-results3 = connection.execute(consulta3).fetchmany(size=10)
-
-for nombreCam,totalAnun in results3:
-    print(nombreCam,totalAnun)
-
-
-
-""" Reporte de los anuncios que tienen imagen o video  """
-
-print("\nReporte de los anuncios que tienen imagen o video \nAnuncio | es imagen o video")
-
-consulta4 = "select anun.Nombre from ANUNCIO anun, TIPO_FORMATO tipf where anun.Tipo_Formato=tipf.ID_formato and (tipf.Es_imagen = true or tipf.Es_video)"
-results4 = connection.execute(consulta4).fetchmany(size=10)
-
-for nombreAnun, in results4:
-    print(nombreAnun)
-
+    while not (opcElegida>=1 and opcElegida<=5):
+        opcElegida=int(input("Seleccione su respuesta: "))
+        if opcElegida == 1:
+            c1.CrearCampana(connection)
+        elif opcElegida == 2:
+            c2.GenerarReporte(connection)
+        elif opcElegida == 3:
+            c3.ActualizarDatos(connection)
+        elif opcElegida == 4:
+            c4.EliminarDatos(connection)
+        elif opcElegida == 5:
+            print("Bye")
 
 connection.close()
