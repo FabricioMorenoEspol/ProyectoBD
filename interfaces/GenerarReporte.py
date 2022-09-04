421b959dcfe8f1453827513545fbfd53ceb83eac
K3vR0ld4n <kroldan@espol.edu.ec>
2022-09-03 19:16:56 -0500

    Cambios en la funcion generasReporte para que consulte solo a los views

--- interfaces/GenerarReporte.py
+++ interfaces/GenerarReporte.py
@@ -14,16 +14,16 @@ def GenerarReporte(connection):
         if opcion == 1:
             print("Reporte del total a pagar por cada Conjunto de Anuncios \nNombre | Total")
 
-            consulta1 = "select CA.nombre, sum(Cantidad_Pago) from ANUNCIO A join CONJUNTO_DE_ANUNCIOS CA on A.ID_Conjunto_De_Anuncios = CA.ID_Conjunto_De_Anuncios join FACTURA F on A.ID_Anuncio=F.ID_Anuncio group by CA.Nombre;"
-            results1 = connection.execute(consulta1).fetchmany(size=10)
+            consulta1 = "select * from ViewTotalPagoCDA"
+            results1 = connection.execute(consulta1).fetchmany(size=20)
 
             for nombre, Total in results1:
                 print(nombre + "----->" + "$" + str(Total))
         elif opcion == 2:
             print("\nReporte del anuncio cuya factura fue la mas alta \nNombre | Total")
 
-            consulta2 = "select A.Nombre, max(F.Cantidad_Pago) from ANUNCIO A join FACTURA F on A.ID_Anuncio=F.ID_Anuncio group by A.Nombre"
-            results2 = connection.execute(consulta2).fetchmany(size=10)
+            consulta2 = "select * from ViewFacturaMasAlta"
+            results2 = connection.execute(consulta2).fetchmany(size=20)
 
             for nombre, Total in results2:
                 print(nombre + "----->" + "$" + str(Total))
@@ -31,16 +31,16 @@ def GenerarReporte(connection):
             print(
                 "\nReporte de cantidad de anuncios publicados por cada campaña \nCampana | Total anuncios publicados ")
 
-            consulta3 = "select cam.Nombre,count(ID_Anuncio) from CAMPAÑA as cam, CONJUNTO_DE_ANUNCIOS as con, ANUNCIO anun where cam.ID_Campaña=con.ID_Campaña and con.ID_Conjunto_De_Anuncios = anun.ID_Conjunto_De_Anuncios group by cam.Nombre"
-            results3 = connection.execute(consulta3).fetchmany(size=10)
+            consulta3 = "select * from ViewNumAnunciosXCampaña"
+            results3 = connection.execute(consulta3).fetchmany(size=20)
 
             for nombreCam, totalAnun in results3:
                 print(nombreCam, totalAnun)
         elif opcion == 4:
             print("\nReporte de los anuncios que tienen imagen o video \nAnuncio | es imagen o video")
 
-            consulta4 = "select anun.Nombre from ANUNCIO anun, TIPO_FORMATO tipf where anun.Tipo_Formato=tipf.ID_formato and (tipf.Es_imagen = true or tipf.Es_video)"
-            results4 = connection.execute(consulta4).fetchmany(size=10)
+            consulta4 = "select * from ViewAnuncioImagenOrVideo"
+            results4 = connection.execute(consulta4).fetchmany(size=20)
 
             for nombreAnun, in results4:
                 print(nombreAnun)
